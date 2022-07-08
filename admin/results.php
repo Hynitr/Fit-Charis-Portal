740fcbe6507f036a92e2f093296245d4018d4bef
Greatness Prodigy <49532326+Grtnxhor@users.noreply.github.com>
2022-07-08 14:45:49 +0100

    result

--- admin/results.php
+++ admin/results.php
@@ -40,16 +40,15 @@ include("functions/top.php");
                         <div class="form-group">
                             <label for="exampleInputPassword1">Select a Class .:</label>
                             <select name="ressbj" id="res" class="form-control">
-                                <option name="class" id="res">Reception</option>
-                                <option name="class" id="res">Transition</option>
-                                <option name="class" id="res">Kindergarten</option>
                                 <option name="class" id="res">Nursery 1</option>
                                 <option name="class" id="res">Nursery 2</option>
-                                <option name="class" id="res">Grade 1</option>
-                                <option name="class" id="res">Grade 2</option>
-                                <option name="class" id="res">Grade 3</option>
-                                <option name="class" id="res">Grade 4</option>
-                                <option name="class" id="res">Grade 5</option>
+                                <option name="class" id="res">KG 1</option>
+                                <option name="class" id="res">KG 2</option>
+                                <option name="class" id="res">Year 1</option>
+                                <option name="class" id="res">Year 2</option>
+                                <option name="class" id="res">Year 3</option>
+                                <option name="class" id="res">Year 4</option>
+                                <option name="class" id="res">Year 5</option>
                                 <option name="class" id="res">J.S.S 1</option>
                                 <option name="class" id="res">J.S.S 2</option>
                                 <option name="class" id="res">J.S.S 3</option>
